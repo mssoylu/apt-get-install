b870e3007855552ade3e76fede65e9f212900475
Mehmet Soylu <3282908+mssoylu@users.noreply.github.com>
2023-10-03 00:15:05 +0300

    Create multiple-composer.sh

---
+++ multiple-composer.sh
@@ -0,0 +1,10 @@
+
+# default version install
+apt install composer
+
+# download alternative version
+wget https://getcomposer.org/download/1.10.17/composer.phar
+
+# how to use alternative version with alternative php version
+/usr/bin/php7.4 composer.phar install
+

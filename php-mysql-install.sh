bbad1181a77379d3d3d8eb905a3f2f05246170eb
Mehmet Soylu <3282908+mssoylu@users.noreply.github.com>
2023-10-02 23:51:35 +0300

    Update php-mysql-install.sh

--- php-mysql-install.sh
+++ php-mysql-install.sh
@@ -17,6 +17,7 @@ FLUSH privileges;
 QUIT
 
 ################
+# PHP & version change
 
 apt install php7.1 php7.1-fpm php7.1-mcrypt php7.1-cli php7.1-mysql php7.1-curl php7.1-xml php7.1-redis php7.1-mbstring
 
@@ -27,3 +28,10 @@ apt install php7.4 php7.4-fpm php7.4-mcrypt php7.4-cli php7.4-mysql php7.4-curl 
 apt install php8.1 php8.1-fpm php8.1-mcrypt php8.1-cli php8.1-mysql php8.1-curl php8.1-xml php8.1-redis php8.1-mbstring
 
 sudo update-alternatives --config php
+
+################
+# Alternative 
+
+sudo a2dismod php7.2
+sudo a2enmod php8.1
+sudo service apache2 restart
